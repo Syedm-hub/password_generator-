03a0cf44cf996cd542d137078645cb4c1ce0d5a9
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-05-22 15:34:08 -0500

    promt added

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
+  let useLowerCase=confirm("Do you want to include lowercase characters?");
+  let useUpperCase=confirm("Do you want to include uppercase characters?");
+  let useNumeric=confirm("Do you want to include numeric characters?");
+  let useSpecial=confirm("Do you want to include special characters?");
+  let passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
